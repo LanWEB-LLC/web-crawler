a465751dde0f10c1c6f31c17164dbe54834913cd
LanWEB-LLC <71795964+LanWEB-LLC@users.noreply.github.com>
2020-10-12 20:39:50 -0400

    Update crawler-indeed.py

--- src/crawler-indeed.py
+++ src/crawler-indeed.py
@@ -1,32 +1,52 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 import requests
 
+count=0
+
+def save_to_file(file_name, contents):
+    fh = open(file_name, 'w', encoding = 'utf-8')
+    fh.write(contents)
+    fh.close()
+
+def getdetail(href):
+    url_detail = 'https://www.indeed.com' + href
+
+    print('url_detail')
+    print(url_detail)
+
+    r1 = requests.get(url_detail)
+
+    soup1 = BeautifulSoup(r1.text, 'lxml')
+
+    description=''
+
+    for divroot in soup1.find_all(attrs={'id':'jobDescriptionText'}):
+        for tag in divroot.descendants:
+            if isinstance(tag, NavigableString):
+                continue
+            else:
+                if len(tag.contents)==1 and isinstance(tag.contents[0], str):
+                    if len(tag.text)==0:
+                        continue
+                    description+=(tag.text+'\n')
+
+    #print(description)
+
+    description = url_detail+'\n'+description
+    save_to_file('E://log'+str(count)+'.txt', description)
 
 url = 'https://www.indeed.com/q-software-engineer-l-USA-jobs.html'
 
-#r = requests.get(url)
+r = requests.get(url)
 
-#soup = BeautifulSoup(r.text, 'lxml')
+soup = BeautifulSoup(r.text, 'lxml')
 
-#print(soup.find_all(attrs={'id':'sja0'}))
+titles = soup.find_all('h2', attrs={'class':'title'})
 
-#a = soup.find_all(attrs={'id':'sja0'})
+for link in titles:
+    if 'href' in link.a.attrs:
+        getdetail(link.a['href'])
+        count+=1
 
-#print(a[0]['href'])
-
-r1 = requests.get('https://www.indeed.com'+'/pagead/clk?mo=r&ad=-6NYlbfkN0ABpo_ZgZOIhs9tMr8Gpr-iBBVTsm4adlixB0fYJReMSldynnTLIcW4QRCDvf4TVHhpMVMAM8TIK3eMyINnd7eMKoEbrt3sO3-wiw01JaiWpQ_pkz2stSdMK_T4A58AudOtaqNAjDqJ2gl5oi8SP7Uc6WTJCCGtcVCxMMytKvTWopUdRJ_UyTa7JmrVVkeFGBwZj1d7q2JExRUWRuacRYmYD8b7mGMgfo9eB36uOL2bRurh88zOGOzMJJrqtYX0OAQYR-wy4VQ5M3oDgemrs56oTxYL6Y1d3JC6o--m2zzBeMTVv1Y12fiIZ3Wx5MwsVnhxfEBW7_2wuI12Xn6tMzD0k3z1EcdofjINtWHaAFD8ONXoTuQt7EE-wmT-PRICFz2cEtJ9C2JbgDsVDtKcs6-5dLE6aYc_NfRBJbB2zXH622oewr5LxEhcu4cerohqK5jTpZTfNuso_w==&p=0&fvj=1&vjs=3')
-
-soup1 = BeautifulSoup(r1.text, 'lxml')
-
-description=''
-
-for c in soup1.find_all(attrs={'id':'jobDescriptionText'})[0].contents:
-    if c.name=='p':
-        description+=(c.text+'\n')
-    if c.name=='ul':
-        for li in c.contents:
-            description+=('   '+li.text+'\n')
-
-print(description)
 
 
